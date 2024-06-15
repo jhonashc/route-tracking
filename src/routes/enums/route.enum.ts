18e289edd5af6f3ddec78c75188c15eb1b0fbc23
jhonashc <jhonhuiracocha123456789@gmail.com>
2024-06-15 14:57:54 -0500

    routes module updated: enumerator for route events added

---
+++ src/routes/enums/route.enum.ts
@@ -0,0 +1,6 @@
+export enum RouteEvent {
+  USER_JOIN = 'route:user-join',
+  DRIVER_JOINED = 'route:driver-joined',
+  SPECTATOR_JOINED = 'route:spectator-joined',
+  STUDENT_JOINED = 'route:student-joined',
+}
